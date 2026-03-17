c332eee4df71069620fdb217747659c3145ba1da
Emilien <emilien.giraud@gottaphish.com>
2026-03-17 17:14:53 +0100

    Ajouter une configuration de délai d'attente pour les requêtes SQLite

--- src/db/index.ts
+++ src/db/index.ts
@@ -9,6 +9,7 @@ const sqlite = new Database(DB_PATH);
 
 sqlite.pragma('journal_mode = WAL');
 sqlite.pragma('foreign_keys = ON');
+sqlite.pragma('busy_timeout = 5000');
 
 export const db = drizzle(sqlite);
 
